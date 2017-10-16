41a61b004904b73e4af592013f7f95b8ba901177
Bart Willems <bwillems@protonmail.com>
2017-10-16 11:59:19 +0200

    Removed useless junk
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- init.sh
+++ init.sh
@@ -1,50 +1,11 @@
 #!/bin/bash
 
-##FUNCTIONS
-function ubuntu {
-  sudo apt update;
-  sudo apt install cmake gcc ctags python2.7-dev vim htop tmux git zsh curl dh-autoreconf wget curl;
-  sudo apt upgrade
-}
+set -e
 
-
-function fedora {
-  sudo dnf install cmake gcc gcc-c++ ctags python-devel vim htop tmux git zsh curl;
-  sudo dnf upgrade
-}
-
-DISTRO=$(lsb_release -a 2>/dev/null | grep 'Distributor' | awk 'END {print $3}')
-USER=$(whoami)
-
-case $DISTRO in
-  ('Ubuntu')
-    ubuntu
-    ;;
-
-  'Fedora' )
-    fedora
-    ;;
-
-  *)
-    ;;
-esac
-
-ln -s $(pwd)/.vim                 /home/$USER/.vim
-ln -s $(pwd)/.vimrc               /home/$USER/.vimrc
-ln -s $(pwd)/.tmux.conf           /home/$USER/.tmux.conf
-ln -s $(pwd)/.config/base16-shell /home/$USER/.config/base16-shell
+ln -s $(pwd)/.vim           $HOME/.vim
+ln -s $(pwd)/.vimrc         $HOME/.vimrc
+ln -s $(pwd)/.tmux.conf     $HOME/.tmux.conf
 
 git submodule update --init --recursive
 
-/home/$USER/.vim/bundle/youcompleteme/install.py
-
-
-dconf write "/org/gnome/desktop/input-sources/xkb-options" "['caps:swapescape']"
-
-wget https://telegram.org/dl/desktop/linux -O telegram.tar.xz && \
-    tar xf telegram.tar.xz && \
-    mkdir -p $HOME/.local/share/applications && \
-    mv Telegram/* $HOME/.local/share/applications/
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "sh $HOME/.config/base16-shell/base16-railscasts.dark.sh" >> /home/$USER/.zshrc
+$HOME/.vim/bundle/youcompleteme/install.py
