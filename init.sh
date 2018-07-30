3002b2d770bd88501abfb313cb3bd431a578a789
Bart Willems <bwillems@protonmail.com>
2018-07-30 20:31:14 +0200

    Removed the submodules
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- init.sh
+++ init.sh
@@ -6,6 +6,3 @@ ln -s $(pwd)/.vim           $HOME/.vim
 ln -s $(pwd)/.vimrc         $HOME/.vimrc
 ln -s $(pwd)/.tmux.conf     $HOME/.tmux.conf
 
-git submodule update --init --recursive
-
-$HOME/.vim/bundle/youcompleteme/install.py
