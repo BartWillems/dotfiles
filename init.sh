d02135858bc29b351db5bc75054f77aeae91468f
Bart Willems <b.willems@wimionline.be>
2019-03-18 12:19:22 +0100

    Start the init script from the repository's location
    Signed-off-by: Bart Willems <b.willems@wimionline.be>

--- init.sh
+++ init.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-ln -s $(pwd)/.vim           $HOME/.vim
-ln -s $(pwd)/.vimrc         $HOME/.vimrc
-ln -s $(pwd)/.tmux.conf     $HOME/.tmux.conf
-ln -s $(pwd)/.config/nvim   $HOME/.config/nvim
+# Make sure this is executed from the repo's directory
+cd "${0%/*}"
+
+ln -s "$(pwd)/.vim"           "$HOME/.vim"
+ln -s "$(pwd)/.vimrc"         "$HOME/.vimrc"
+ln -s "$(pwd)/.tmux.conf"     "$HOME/.tmux.conf"
+ln -s "$(pwd)/.config/nvim"   "$HOME/.config/nvim"
 
